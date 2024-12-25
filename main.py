885b5ecd43401dcc32659b4531313bb20da2c88a
son nguyen <son.9b.2015@gmail.com>
2024-12-25 20:34:43 +0700

    source

---
+++ main.py
@@ -0,0 +1,84 @@
+import asyncio
+from typing import Annotated
+from fastapi.responses import JSONResponse
+from sklearn.cluster import KMeans
+import random as rng
+import cv2
+import imutils
+import argparse
+from imutils import contours
+# from skimage.io import imread => replace to imageio.v3
+import imageio.v3 as iio
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from utils import *
+
+
+ImgPath = 'data/barefeet_4.jpg'
+
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow all origins 
+origins = [
+    "http://localhost",  # allow frontend running on localhost
+    "http://localhost:8000",  # allow frontend on FastAPI dev server
+]
+
+# CORS to app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
+
+@app.post("/")
+async def read_root(file: Annotated[bytes, File()]): # file: UploadFile = File(...)
+    feetSize = round(getSize(file))
+    await asyncio.sleep(2)
+    return JSONResponse(content={"size": feetSize})
+
+def getSize(input_image):
+
+    # oimg = imread(ImgPath)
+    # oimg = iio.imread(ImgPath)
+    oimg = iio.imread(input_image)
+
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+
+
+    preprocessedOimg = preprocess(oimg)
+    cv2.imwrite('output/preprocessedOimg.jpg', preprocessedOimg)
+
+    clusteredImg = kMeans_cluster(preprocessedOimg)
+    cv2.imwrite('output/clusteredImg.jpg', clusteredImg)
+
+    edgedImg = edgeDetection(clusteredImg)
+    cv2.imwrite('output/edgedImg.jpg', edgedImg)
+
+    boundRect, contours, contours_poly, img = getBoundingBox(edgedImg)
+    pdraw = drawCnt(boundRect[1], contours, contours_poly, img)
+    cv2.imwrite('output/pdraw.jpg', pdraw)
+
+    croppedImg, pcropedImg = cropOrig(boundRect[1], clusteredImg)
+    cv2.imwrite('output/croppedImg.jpg', croppedImg)
+    
+
+    newImg = overlayImage(croppedImg, pcropedImg)
+    cv2.imwrite('output/newImg.jpg', newImg)
+
+    fedged = edgeDetection(newImg)
+    fboundRect, fcnt, fcntpoly, fimg = getBoundingBox(fedged)
+    fdraw = drawCnt(fboundRect[2], fcnt, fcntpoly, fimg)
+    cv2.imwrite('output/fdraw.jpg', fdraw)
+
+    return  calcFeetSize(pcropedImg, fboundRect)/10
